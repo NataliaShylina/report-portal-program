b5a93501db6c6036d886aff02fd9dd2f076c0cc5
Nataliia Shylina <nataliia_shylina@epam.com>
2023-06-18 20:22:43 +0200

    TAF design. 11 version. UI test 1.Configured Waits for web elements.
    2. Implement screenshot taker utility.

--- src/test/java/org/example/base/selenide/LaunchesTest.java
+++ src/test/java/org/example/base/selenide/LaunchesTest.java
@@ -1,9 +1,17 @@
 package org.example.base.selenide;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import lombok.extern.log4j.Log4j2;
+import org.example.domain.Credentials;
+import org.example.domain.UserType;
+import org.example.provider.CredentialsProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.By;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -16,11 +24,22 @@ class LaunchesTest {
     private static final String TOTAL_FIELD_TEMPLATE = ".//a[@statuses= \"PASSED,FAILED,SKIPPED,INTERRUPTED\"]";
 
     private static final String PASSED_FIELD_TEMPLATE = ".//a[@statuses= \"PASSED\"]";
+    private static final Duration DURATION_TIMEOUT = Duration.ofSeconds(5);
+
+    @RegisterExtension
+    static ScreenShooterExtension screenshotEmAll = new ScreenShooterExtension().to("target/screenshots");
+
     @Test
     void verifyThatLaunchesContainsExpectedData() {
+        Credentials credentials = new CredentialsProvider().provideByUserType(UserType.DEFAULT_USER);
+
         open(BASE_PATH + "ui/#login");
-        $(By.xpath("//input[@placeholder='Login']")).setValue("default");
-        $(By.xpath("//input[@placeholder='Password']")).setValue("1q2w3e");
+        $(By.xpath("//input[@placeholder='Login']"))
+                .should(Condition.visible, DURATION_TIMEOUT)
+                .setValue(credentials.getLogin());
+        $(By.xpath("//input[@placeholder='Password']"))
+                .should(Condition.visible, DURATION_TIMEOUT)
+                .setValue(credentials.getPassword());
         $(By.xpath("//button[normalize-space()='Login']")).click();
         $(By.xpath("(//a[@href='#default_personal/launches'])")).click();
         SelenideElement blockElement = $(By.xpath(String.format(LAUNCH_SIDE_GRID_NAME_TEMPLATE, 10)));
